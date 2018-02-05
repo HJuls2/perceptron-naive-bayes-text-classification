ae383d3347ab3c779dc0cc677185d3a7040b6a88
HJuls <giuliobaronti@gmail.com>
2018-02-05 12:13:29 +0100

    update percetron

--- Perceptron.py
+++ Perceptron.py
@@ -1,31 +1,29 @@
 
+
 import operator
 
 
 class Percetron:
 
-    def __init__(self, s,r,rate):
+    def __init__(self,s,r,rate):
         self.train=s
         self.results=r
         self.rate=rate
-        self.weights=list({0})
-        self.bias=list({0})
+        for doc in self.train:
+            self.weights[doc]=0
+            self.bias[doc]=0
         self.errors=0
         self.r=max(self.train.iteritems(), key=operator.itemgetter(1))[0]
 
 
-    def run(self):
-        for doc  in self.train:
-            temp=0
-            for w in self.weights:
-                temp=temp+w*
+    def run(self,train,results,rate):
+        for doc in self.train:
             if(self.results[doc]*(self.weight*doc+self.bias)<=0):
-                self.weights.append
-                [errors+1]=self.weight[error]+self.rate*self.results[word]*doc.
-                errors=errors+1
+                self.weight=self.weight+self.rate*self.results[doc]*doc
+                self.bias=self.bias+self.bias+self.rate*self.results[doc]*(self.r*self.r)
+                self.errors=self.errors+1
 
-
-
+        return self.weight,self.bias
 
 
 
