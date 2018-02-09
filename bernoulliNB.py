ac39fa8090f1651a7ac9a587eab97ee84243dc0d
HJuls <giuliobaronti@gmail.com>
2018-02-09 10:38:33 +0100

    reverted bernoulliNB.py
    bernoulli train reverted to single thread version
    (multiple thread one avalaible on thread-version branch)

--- bernoulliNB.py
+++ bernoulliNB.py
@@ -1,57 +1,77 @@
 import manager
-import _thread
-from math  import log10
-from numpy import argmax, sort
-from nltk.corpus import reuters
-from manager import tokenize
 
-def calc_condprob(condprob,vocabulary,c,doc_in_class):
-    print("thread "+c+" started")
-    condprob=dict()
-    for word in vocabulary:
-            occur=0
-            for doc in reuters.fileids(c):
-                doc_index=reuters.fileids(c).index(doc)
-                #parole del documento corrente
-                doc_words=sort(tokenize(reuters.raw(reuters.fileids(c)[doc_index])))
-                #doc_words=list(set(reuters.words(reuters.fileids(c)[doc_index])))
-                if(word in doc_words):
-                    occur+=1
-                condprob[(word,c)]=(occur+1)/(doc_in_class+2)
-    
+from math  import log10
+
+from numpy import argmax
+
+from nltk.corpus import reuters
+
+
 
 def train_bernoulli():
+
     vocabulary=manager.extractVocabulary()
+
     numdocs=len(reuters.fileids())
+
     prior=dict()
+
     condprob=dict()
+
     for c in reuters.categories():
-        print("start with category "+c)
+
         doc_in_class=len(reuters.fileids(c))
+
         prior[c]=doc_in_class/numdocs
-        _thread.start_new_thread(calc_condprob, (condprob,vocabulary,c,doc_in_class))
-        
-    return prior,condprob
-    
-    
+
+        for word in vocabulary:
+
+            occur=0
+
+            for doc in reuters.fileids(c):
+
+                doc_index=reuters.fileids(c).index(doc)
+
+                #parole del documento corrente
+
+                doc_words=reuters.words(reuters.fileids(c)[doc_index])
+
+                if(word in doc_words):
+
+                    occur+=1
+
+            condprob[(word,c)]=(occur+1)/(doc_in_class+2)
+
+        return prior,condprob
+
     
 
+    
+
+
+
 def applyBernoulli(doc,prior,condprob):
+
     voc=manager.extractVocabulary()
+
     doc_voc=reuters.words(doc)
+
     score=dict()
+
     for c in reuters.categories(doc):
+
         score[c]=log10(prior[c])
+
         for word in voc:
+
             if(word in doc_voc):
+
                 score[c]+=log10(condprob(word,c))
+
             else:
+
                 score[c]*=log10(1-condprob(word,c))
+
     
+
     return argmax(score)
-            
-        
-    
-    
-           
-    
