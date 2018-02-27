2a525d34372d10c6a9b44bffa9da2b8232af6ca3
HJuls <giuliobaronti@gmail.com>
2018-02-27 10:30:48 +0100

    fixed bernoulliNB.py

--- bernoulliNB.py
+++ bernoulliNB.py
@@ -1,35 +1,47 @@
 import manager
 
 from math  import log10
-
 from numpy import argmax
+
 
 from nltk.corpus import reuters
 from builtins import range
 
 
 
-def train_bernoulli(vocabulary):
-    numdocs=len(reuters.fileids())
+def train_bernoulli(train_set,categories,vocabulary):
+    numdocs=len(train_set)
     prior=dict()
     condprob=dict()
     
-    for c in reuters.categories():
-        doc_in_class=len(reuters.fileids(c))
-        prior[c]=doc_in_class/numdocs
+    for c in categories:
+        doc_in_class=sorted(list(doc for doc in train_set if doc in reuters.fileids(c)))
+        numdocsinclass=len(doc_in_class)
+        print(numdocsinclass)
+        prior[c]=numdocsinclass/numdocs
+        
+        text=str()
+        for doc in doc_in_class:
+            text+=reuters.raw(doc)
+        words_in_class=sorted(manager.extractVocabulary(text))
+        print(len(words_in_class))
         
         for word in vocabulary:
-            occur=len([doc for doc in reuters.categories(c) if word in manager.porterStemmer(list(set(reuters.words(reuters.fileids(c)[reuters.categories(c)[reuters.categories(c).index(doc)]]))))])
-            condprob[(word,c)]=(occur+1)/(doc_in_class+2)
+            if word in words_in_class:
+                occur=len([doc for doc in doc_in_class if word in words_in_class])
+                words_in_class.remove(word)
+                condprob[(word,c)]=(occur+1)/(numdocsinclass+2)
+            else:
+                condprob[(word,c)]=1/(numdocsinclass+2)
             
     return prior,condprob
 
 
-def applyBernoulli(vocabulary,doc,prior,condprob):
+def applyBernoulli(vocabulary,categories,doc,prior,condprob):
     print("#### "+doc+ " ####")
     doc_voc=manager.extractVocabulary(reuters.raw(doc))
     score=dict()
-    for c in reuters.categories():
+    for c in categories:
         score[c]=log10(prior[c])
         for word in vocabulary:
             if(word in doc_voc):
--- test.py
+++ test.py
@@ -16,18 +16,18 @@ def main():
     for t in train_docs:
         text+=reuters.raw(t)
         
-    vocabulary=extractVocabulary(text);
-    prior,condprob=train_bernoulli(vocabulary);
+    vocabulary=sorted(extractVocabulary(text));
+    prior,condprob=train_bernoulli(train_docs,categories,vocabulary);
     
+    print(condprob)
     results=np.zeros(len(test_docs))
     for d in test_docs:
-        cat=(cat for cat in reuters.categories() if d in cat)
-        calcCat=applyBernoulli(vocabulary, d, prior, condprob)
+        cat=(cat for cat in categories if d in reuters.fileids(cat))
+        calcCat=applyBernoulli(vocabulary,categories, d, prior, condprob)
         if(calcCat==cat):
             np.insert(results, d, 1)
             
-    print(results)
-            
+    print(results)   
 
 if __name__ == "__main__":
     main()
